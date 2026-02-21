8de86d42c6a462215dd07fe56667bad08b580ec4
Cassius310 <cmorales@usv.edu>
2026-02-20 18:33:34 -0800

    blockOut
    added omni, restructured and scaled. Fixed controller

--- AddOns/proto_controller/proto_controller.gd
+++ AddOns/proto_controller/proto_controller.gd
@@ -22,7 +22,7 @@ extends CharacterBody3D
 ## Normal speed.
 @export var base_speed : float = 10.0
 ## Speed of jump.
-@export var jump_velocity : float = 4.5
+@export var jump_velocity : float = 6
 ## How fast do we run?
 @export var sprint_speed : float = 10.0
 ## How fast do we freefly?
@@ -38,7 +38,7 @@ extends CharacterBody3D
 ## Name of Input Action to move Backward.
 @export var input_back : String = "move_down"
 ## Name of Input Action to Jump.
-@export var input_jump : String = "ui_accept"
+@export var input_jump : String = "jump"
 ## Name of Input Action to Sprint.
 @export var input_sprint : String = "sprint"
 ## Name of Input Action to toggle freefly mode.
